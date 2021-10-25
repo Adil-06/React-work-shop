885a4cf6cacfdec72b6e6a221ed34d582d8adc91
adil-06 <mustafakhanadil@gmail.com>
2021-10-25 19:51:19 +0500

    event booking app
    an event booking app
    models added
    project setup
    graphql setup

---
+++ event-booking-app/event-booking-graphql/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const graphqlHttp = require('express-graphql');
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use('/graphql', graphqlHttp.graphqlHTTP({
+  schema: graphqlSchema,
+  rootValue: graphqlResolver,
+  graphiql: true
+}));
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_CONNECTION,
+  { useNewUrlParser: true,  useUnifiedTopology: true }
+).then(() => { console.log("db connected") })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(8080, () => {
+  console.log('server running on 8080');
+})
---
+++ event-booking-app/event-booking-graphql/models/event.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const eventSchema = mongoose.Schema({
+    title: {
+      type : String,
+      require: true
+    },
+    description: {
+      type: String,
+      require: true
+    },
+    price: {
+      type: Number,
+      require: true
+    },
+    date: {
+      type : Date,
+      require: true
+    },
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+});
+module.exports = mongoose.model('Event', eventSchema);
